9b050caad9f685e50f50d58d143df869cd5145ed
pallavipawar4034-hue <pallavipawar4034@gmail.com>
2026-07-25 04:15:12 -0700

    Add required attribute to city input in search form
    Updated the index.php file to include a required attribute for the city input field in the search form.

--- index.php
+++ index.php
@@ -8,24 +8,19 @@ session_start();
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Welcome | PG Life</title>
-
-    <?php
-    include "includes/head_links.php";
-    ?>
+    <?php include "includes/head_links.php"; ?>
     <link href="css/home.css" rel="stylesheet" />
 </head>
 
 <body>
-    <?php
-    include "includes/header.php";
-    ?>
+    <?php include "includes/header.php"; ?>
 
     <div class="banner-container">
         <h2 class="white pb-3">Happiness per Square Foot</h2>
 
         <form id="search-form" action="property_list.php" method="GET">
             <div class="input-group city-search">
-                <input type="text" class="form-control input-city" id='city' name='city' placeholder="Enter your city to search for PGs" />
+                <input type="text" class="form-control input-city" id='city' name='city' placeholder="Enter your city to search for PGs" required />
                 <div class="input-group-append">
                     <button type="submit" class="btn btn-secondary">
                         <i class="fa fa-search"></i>
@@ -36,39 +31,26 @@ session_start();
     </div>
 
     <div class="page-container">
-        <h1 class="city-heading">
-            Major Cities
-        </h1>
+        <h1 class="city-heading">Major Cities</h1>
         <div class="row">
             <div class="city-card-container col-md">
                 <a href="property_list.php?city=Delhi">
-                    <div class="city-card rounded-circle">
-                        <img src="img/delhi.png" class="city-img" />
-                    </div>
+                    <div class="city-card rounded-circle"><img src="img/delhi.png" class="city-img" /></div>
                 </a>
             </div>
-
             <div class="city-card-container col-md">
                 <a href="property_list.php?city=Mumbai">
-                    <div class="city-card rounded-circle">
-                        <img src="img/mumbai.png" class="city-img" />
-                    </div>
+                    <div class="city-card rounded-circle"><img src="img/mumbai.png" class="city-img" /></div>
                 </a>
             </div>
-
             <div class="city-card-container col-md">
                 <a href="property_list.php?city=Bengaluru">
-                    <div class="city-card rounded-circle">
-                        <img src="img/bangalore.png" class="city-img" />
-                    </div>
+                    <div class="city-card rounded-circle"><img src="img/bangalore.png" class="city-img" /></div>
                 </a>
             </div>
-
             <div class="city-card-container col-md">
                 <a href="property_list.php?city=Hyderabad">
-                    <div class="city-card rounded-circle">
-                        <img src="img/hyderabad.png" class="city-img" />
-                    </div>
+                    <div class="city-card rounded-circle"><img src="img/hyderabad.png" class="city-img" /></div>
                 </a>
             </div>
         </div>
@@ -79,7 +61,6 @@ session_start();
     include "includes/login_modal.php";
     include "includes/footer.php";
     ?>
-
 </body>
-
 </html>
+
