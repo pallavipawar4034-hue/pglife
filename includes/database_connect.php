587f712a02c0efb5eab3eb260797a7f37cafedd4
pallavipawar4034-hue <pallavipawar4034@gmail.com>
2026-07-25 04:43:48 -0700

    Update database_connect.php

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,1 +1,14 @@
+<?php
+$db_host = "sql306.infinityfree.com";
+$db_user = "if0_42487701";
+$db_pass = "Survi2105"; 
+$db_name = "if0_42487701_pglife";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
+
 
